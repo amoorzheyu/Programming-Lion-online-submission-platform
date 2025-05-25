33cc58eb0265cfc317cd2f84a1c6325b5692b65d
amoorzheyu <szc8848@qq.com>
2025-05-25 12:10:13 +0800

    feat: 新增了名单分离模块

---
+++ contentStudentData.js
@@ -0,0 +1,90 @@
+let contentStudentData = {
+  //  0: {name:'xxx',data:'',isReset:true},
+  // 0: {name:'余俊翼',data:'',isReset:true},
+  // 0:{name:'陆沛林',data:'',isReset:true},
+  // 2:{name:'黄梓涵',data:'',isReset:true},
+  // 0:{name:'陆沛林',data:'',isReset:true},
+
+  // 0: { name: "苏俞华", data: "", isReset: true },
+
+  // 0:{name:"慎梓睿",data:"",isReset:true},
+  // 1:{name:"罗耀隆",data:"",isReset:true},
+  // 0:{name:'孙铱辰',data:'',isReset:true},
+  // 1:{name:'陶元钦',data:'',isReset:true},
+
+  // 0: {name:'杜睿谦',data:'',isReset:true},
+  // 1:{name:'陶元钦',data:'',isReset:true},
+
+  // 0:{name:'黄熙粤',data:'',isReset:true},
+  // 1:{name:'胡哲涵',data:'',isReset:true},
+
+  // 0: { name: "徐远涵", data: "", isReset: true },
+  // 1: { name: "杨政", data: "", isReset: true },
+  // 2:{name:'聂可馨',data:'',isReset:true},
+
+  // 0: {name:'徐远涵',data:'',isReset:true},
+  // 1:{name:'聂可馨',data:'',isReset:true},
+
+  // 0: { name: '陈少宗', data: '', isReset: true },
+  // 1: { name: '林冠廷', data: '' ,isReset: true },
+  // 2: { name: '陈泽昊', data: '', isReset: true },
+
+  // 0: { name: '姜希杰', data: '' ,isReset:true},
+  // 1: { name: '罗耀隆', data: '' ,isReset:true},
+  // 2: { name: '谢铠烨', data: '' ,isReset:true},
+  // 3: { name: '谢铠磊', data: '' ,isReset:true},
+  // 4: { name: '邓景珑', data: '' ,isReset:true},
+
+  // 0: { name: '邓景珑', data: '' ,isReset:true},
+
+  // 0: { name: '邓景珑', data: '' ,isReset:true},
+  // 1: { name: '谢铠烨', data: '' ,isReset:true},
+  // 2: { name: '谢铠磊', data: '' ,isReset:true},
+
+  // 0: { name: '罗耀隆', data: '' ,isReset:true},
+  // 1: { name: '林俊漩', data: '' ,isReset:true},
+  // 2: { name: '慎梓睿', data: '' ,isReset:true},
+  // 3: { name: '郑博阳', data: '' ,isReset:true},
+
+  // 0: { name: "慎梓睿", data: "", isReset: true },
+  // 1: { name: "黄熙粤", data: "", isReset: true },
+  // 2: { name: '姜希杰', data: '' ,isReset:true},
+
+
+  // -----------周一-----------下午正常名单----------------------------
+  // 0: {name:'杜睿谦',data:'',isReset:true},
+  // 1:{name:'孙铱辰',data:'',isReset:true},
+  // 2:{name:'陶元钦',data:'',isReset:true},
+
+  // -----------周六-----------下午第三节课正常名单----------------------------
+  // 0: { name: '罗耀隆', data: '' ,isReset:true},
+  // 1: { name: '慎梓睿', data: '' ,isReset:true},
+  // 2: { name: '林俊漩', data: '' ,isReset:true},
+  // 3: { name: '谢铠烨', data: '' ,isReset:true},
+  // 4: { name: '谢铠磊', data: '' ,isReset:true},
+
+  // -----------周六-----------晚上正常名单----------------------------
+  // 0:{name:'黄梓涵',data:'',isReset:true},
+  // 1:{name:'杨梓悦',data:'',isReset:true},
+
+  // ----------周日-----------上午第一节正常名单----------------------------
+  0: { name: "罗耀隆", data: "", isReset: true },
+  1: { name: "林俊漩", data: "", isReset: true },
+  // 2: { name: "慎梓睿", data: "", isReset: true },
+  // 3: { name: "谢铠烨", data: "", isReset: true },
+  // 4: { name: "谢铠磊", data: "", isReset: true },
+
+  // ----------周日-----------上午第二节正常名单----------------------------
+  // 0: { name: "姜希杰", data: "", isReset: true },
+  // 1: { name: "邓景珑", data: "", isReset: true },
+
+  // ----------周日-----------下午第1、2节正常名单----------------------------
+  // 0: { name: "彭雄晖", data: "", isReset: true },
+  // 1: { name: "陆沛林", data: "", isReset: true },
+}
+
+let getContentStudentData = () => {
+  return contentStudentData;
+};
+
+module.exports = { getContentStudentData };
